667e9cfe06e0520f3b75e8b21dff019d76a1799a
MateoLescano <mateolescano@gmail.com>
2021-08-23 14:02:08 -0300

    commit inicial

--- app/src/main/java/com/citesoftware/generadorderelatos/MainActivity.kt
+++ app/src/main/java/com/citesoftware/generadorderelatos/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.citesoftware.generadorderelatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Switch
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+
+        findViewById<Button>(R.id.btnGenerar).setOnClickListener {
+
+            val RoF = findViewById<Switch>(R.id.switchFantasia).isChecked
+            val Momento = findViewById<Switch>(R.id.switchMomento).isChecked
+            val Ubicacion = findViewById<Switch>(R.id.switchUbicacion).isChecked
+            val Personaje = findViewById<Switch>(R.id.switchPersonaje).isChecked
+            val Accion = findViewById<Switch>(R.id.switchAccion).isChecked
+            val Objeto = findViewById<Switch>(R.id.switchObjeto).isChecked
+            val Detalle = findViewById<Switch>(R.id.switchDetalle).isChecked
+
+
+
+            val intent = Intent(this, RelatoActivity::class.java)
+
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//            intent.putExtra("RoF", RoF)
+            intent.putExtra("Momento", Momento)
+            intent.putExtra("Ubicacion", Ubicacion)
+            intent.putExtra("Personaje", Personaje)
+            intent.putExtra("Accion", Accion)
+            intent.putExtra("Objeto", Objeto)
+            intent.putExtra("Detalle", Detalle)
+
+            this.startActivity(intent)
+        }
+
     }
+
+
 }
--- app/src/main/java/com/citesoftware/generadorderelatos/RelatoActivity.kt
+++ app/src/main/java/com/citesoftware/generadorderelatos/RelatoActivity.kt
@@ -2,10 +2,48 @@ package com.citesoftware.generadorderelatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class RelatoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relato)
+
+        if(intent.getBooleanExtra("Momento", false)){
+            findViewById<TextView>(R.id.tvMomentoGenerado).text = "Si pa"
+        }else{
+            findViewById<TextView>(R.id.tvMomentoGenerado).text = "No pa"
+        }
+
+        if(intent.getBooleanExtra("Ubicacion", false)){
+            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "Si pa"
+        }else{
+            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "No pa"
+        }
+
+        if(intent.getBooleanExtra("Personaje", false)){
+            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "Si pa"
+        }else{
+            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "No pa"
+        }
+
+        if(intent.getBooleanExtra("Accion", false)){
+            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "Si pa"
+        }else{
+            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "No pa"
+        }
+
+        if(intent.getBooleanExtra("Objeto", false)){
+            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "Si pa"
+        }else{
+            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "No pa"
+        }
+
+        if(intent.getBooleanExtra("Detalle", false)){
+            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "Si pa"
+        }else{
+            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "No pa"
+        }
     }
 }
