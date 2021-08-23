6f15e766b2e4d8ba0b3ea69c20f45e31963c9e83
MateoLescano <mateolescano@gmail.com>
2021-08-23 13:16:40 -0300

    commit inicial

---
+++ app/src/main/java/com/citesoftware/generadorderelatos/RelatoActivity.kt
@@ -0,0 +1,11 @@
+package com.citesoftware.generadorderelatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelatoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relato)
+    }
+}
