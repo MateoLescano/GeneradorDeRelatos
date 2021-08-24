9ec5304e7c277fd9396dc40e36c873604a82d15e
MateoLescano <mateolescano@gmail.com>
2021-08-24 10:49:17 -0300

    24/8

--- app/src/main/java/com/citesoftware/generadorderelatos/RelatoActivity.kt
+++ app/src/main/java/com/citesoftware/generadorderelatos/RelatoActivity.kt
@@ -2,6 +2,7 @@ package com.citesoftware.generadorderelatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
 import android.widget.Button
 import android.widget.TextView
 
@@ -13,37 +14,37 @@ class RelatoActivity : AppCompatActivity() {
         if(intent.getBooleanExtra("Momento", false)){
             findViewById<TextView>(R.id.tvMomentoGenerado).text = "Si pa"
         }else{
-            findViewById<TextView>(R.id.tvMomentoGenerado).text = "No pa"
+            findViewById<TextView>(R.id.tvMomentoGenerado).visibility = GONE
         }
 
         if(intent.getBooleanExtra("Ubicacion", false)){
             findViewById<TextView>(R.id.tvUbicacionGenerado).text = "Si pa"
         }else{
-            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "No pa"
+            findViewById<TextView>(R.id.tvUbicacionGenerado).visibility = GONE
         }
 
         if(intent.getBooleanExtra("Personaje", false)){
-            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "Si pa"
+            findViewById<TextView>(R.id.tvPersonajeGenerado).text = "Si pa"
         }else{
-            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "No pa"
+            findViewById<TextView>(R.id.tvPersonajeGenerado).visibility = GONE
         }
 
         if(intent.getBooleanExtra("Accion", false)){
-            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "Si pa"
+            findViewById<TextView>(R.id.tvAccionGenerado).text = "Si pa"
         }else{
-            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "No pa"
+            findViewById<TextView>(R.id.tvAccionGenerado).visibility = GONE
         }
 
         if(intent.getBooleanExtra("Objeto", false)){
-            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "Si pa"
+            findViewById<TextView>(R.id.tvObjetoGenerado).text = "Si pa"
         }else{
-            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "No pa"
+            findViewById<TextView>(R.id.tvObjetoGenerado).visibility = GONE
         }
 
         if(intent.getBooleanExtra("Detalle", false)){
-            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "Si pa"
+            findViewById<TextView>(R.id.tvDetalleGenerado).text = "Si pa"
         }else{
-            findViewById<TextView>(R.id.tvUbicacionGenerado).text = "No pa"
+            findViewById<TextView>(R.id.tvDetalleGenerado).visibility = GONE
         }
     }
 }
