5597b6ce618cc4d5e12af507af5e01b956cb4de7
MateoLescano <mateolescano@gmail.com>
2021-09-09 16:46:39 -0300

    9/9

--- app/src/main/java/com/citesoftware/generadorderelatos/RelatoActivity.kt
+++ app/src/main/java/com/citesoftware/generadorderelatos/RelatoActivity.kt
@@ -130,10 +130,66 @@ class RelatoActivity : AppCompatActivity() {
 
             when(tiempo){
 
-                1 -> lista = listOf("Invención de la escritura","Antiguo Egipto","Construcción de las pirámides de Giza","Antigua Grecia","Construcción del partenón","Imperio Romano","Imperio Maya","Juegos olímpicos originales","Erupción del Monte Vesubio.","Juicio a Sócrates.","Renacimiento.","Imperio Otomano.","Conquista de América.","Caída del imperio bizantino.","Guerra de los 100 años.","Feudalismo.","La peste negra.","La época de construcción de templos budistas en Asia", "La revolución de Haití.", "Un universo donde se fuma marihuana para estar sobrio")
-
-                2, 3 -> lista = listOf("La revolución Francesa.","Declaración de la independencia Estadounidense","La revolución de Haití", "Dictadura Chilena en 1970.","Dictadura Argentina en 1976.","Crisis Argentina del 2001.","Gobierno de Menem.","Tercer gobierno Peronista.","Gobierno de Juan Manuel de Rosas.","Revolución Rusa.","Revolución Cubana.","Revolución de los Claveles.","Epoca del cine mudo.","Crisis economica del 30.","Segunda guerra mundial.","Primera guerra mundial.","Invención del smartphone.","Inteligencia artificial.","Epidemia del HIV.","Caída del muro de Berlín.","Crisis del 2008.","Crisis Española","La bomba atómica de Hiroshima/Nagasaki.","Chernobyl.","Guerra fría.","Hippismo.","Era dorada del punk.","Pandemia Covid-19.","Asesinato de John F. Kennedy.","Atentado de las torres gemelas.","El hombre llega a la Luna.", "Invención del televisor.","Estreno de 'El padrino'.", "Estreno de 'Star Wars'.","Construcción del Teatro Colón.", "La mañana que Bob Marley escribió No Woman No Cry","Tercera guerra mundial.","Climax de la crisis climática.","Asesinato de un presidente de la República Argentina.","Primer excursión a Marte.","Guerra del agua.","Holocausto nuclear.","Colonización del primer planeta del Imperio galáctico humano.","Los polos tras el calentamiento global siendo los únicos lugares habitables por humanos que quedan","Descubren vida extraterrestre, año 2038", "Un universo donde se fuma marihuana para estar sobrio")
-
+                1 -> lista = listOf(getString(R.string.momento2),
+                    getString(R.string.momento3),
+                    getString(R.string.momento4),
+                    getString(R.string.momento6),
+                    getString(R.string.momento7),
+                    getString(R.string.momento8),
+                    getString(R.string.momento9),
+                    getString(R.string.momento10),
+                    getString(R.string.momento11),
+                    getString(R.string.momento12),
+                    getString(R.string.momento14),
+                    getString(R.string.momento15),
+                    getString(R.string.momento16),
+                    getString(R.string.momento20),
+                    getString(R.string.momento51))
+                2, 3 -> lista = listOf(
+                    getString(R.string.momento17),
+                    getString(R.string.momento18),
+                    getString(R.string.momento19),
+                    getString(R.string.momento21),
+                    getString(R.string.momento22),
+                    getString(R.string.momento23),
+                    getString(R.string.momento24),
+                    getString(R.string.momento25),
+                    getString(R.string.momento26),
+                    getString(R.string.momento27),
+                    getString(R.string.momento28),
+                    getString(R.string.momento29),
+                    getString(R.string.momento30),
+                    getString(R.string.momento31),
+                    getString(R.string.momento32),
+                    getString(R.string.momento33),
+                    getString(R.string.momento34),
+                    getString(R.string.momento35),
+                    getString(R.string.momento36),
+                    getString(R.string.momento37),
+                    getString(R.string.momento38),
+                    getString(R.string.momento39),
+                    getString(R.string.momento40),
+                    getString(R.string.momento41),
+                    getString(R.string.momento42),
+                    getString(R.string.momento43),
+                    getString(R.string.momento44),
+                    getString(R.string.momento46),
+                    getString(R.string.momento47),
+                    getString(R.string.momento48),
+                    getString(R.string.momento49),
+                    getString(R.string.momento50),
+                    getString(R.string.momento51),
+                    getString(R.string.momento52),
+                    getString(R.string.momento53),
+                    getString(R.string.momento54),
+                    getString(R.string.momento55),
+                    getString(R.string.momento56),
+                    getString(R.string.momento57),
+                    getString(R.string.momento58),
+                    getString(R.string.momento59),
+                    getString(R.string.momento60),
+                    getString(R.string.momento61),
+                    getString(R.string.momento62))
             }
 
             val randint = Random.nextInt(lista.size)
@@ -216,10 +272,78 @@ class RelatoActivity : AppCompatActivity() {
 
             when(tiempo){
 
-                1 -> lista = listOf( "Un calabozo", "Una casa alejada de todo", "Un bunker", "Un hospital", "Lisboa, Portugal", "Una ciudad desconocida", "Monte Fuji", "Dinamarca", "Una iglesia, templo o mesquita", "una taberna", "la mitad de la carretera", "Praga", "Roma", "Damasco", "Atenas, Grecia", "Cuzco", "Las piramides Egipcias")
-
-                2, 3 -> lista = listOf("Ciudad de Buenos Aires", "Una terminal de colectivos", "Una estación de tren","Un café", "Un bagon de tren abandonado", "El andén 2 de la estación de Retiro", "Una casa alejada de todo", "Un bunker", "Praga, Republica Checa", "Un hospital", "Pyongyang, Corea del Norte", "Seúl, Corea del Sur", "Monte Fuji", "Hong Kong", "Un lugar de altura muy elevada", "Un parque de diversiones", "Un sitio abandonado", "Salto angel, Venezuela", "Un tren en movimiento", "Una ciudad desconocida", "San Petesburgo, Rusia", "Ciudad del Cabo, Sudafrica", "Estanbul, Turquía", "Lisboa, Portugal", "Dinamarca", "Una iglesia, templo o mesquita",  "la mitad de la carretera", "Praga", "Roma", "Damasco", "Atenas, Grecia", "Cuzco", "Las piramides Egipcias")
-
+                1 -> lista = listOf(
+                    getString(R.string.ubicacion1),
+                    getString(R.string.ubicacion2),
+                    getString(R.string.ubicacion4),
+                    getString(R.string.ubicacion7),
+                    getString(R.string.ubicacion11),
+                    getString(R.string.ubicacion13),
+                    getString(R.string.ubicacion14),
+                    getString(R.string.ubicacion17),
+                    getString(R.string.ubicacion19),
+                    getString(R.string.ubicacion21),
+                    getString(R.string.ubicacion22),
+                    getString(R.string.ubicacion27),
+                    getString(R.string.ubicacion28),
+                    getString(R.string.ubicacion29),
+                    getString(R.string.ubicacion30),
+                    getString(R.string.ubicacion32),
+                    getString(R.string.ubicacion33),
+                    getString(R.string.ubicacion34),
+                    getString(R.string.ubicacion35),
+                    getString(R.string.ubicacion36),
+                    getString(R.string.ubicacion38),
+                    getString(R.string.ubicacion40),
+                    getString(R.string.ubicacion41),
+                    getString(R.string.ubicacion42),
+                    getString(R.string.ubicacion43),
+                    getString(R.string.ubicacion44)
+                )
+                2, 3 -> lista = listOf(
+                    getString(R.string.ubicacion1),
+                    getString(R.string.ubicacion2),
+                    getString(R.string.ubicacion4),
+                    getString(R.string.ubicacion7),
+                    getString(R.string.ubicacion11),
+                    getString(R.string.ubicacion13),
+                    getString(R.string.ubicacion14),
+                    getString(R.string.ubicacion17),
+                    getString(R.string.ubicacion19),
+                    getString(R.string.ubicacion21),
+                    getString(R.string.ubicacion22),
+                    getString(R.string.ubicacion27),
+                    getString(R.string.ubicacion28),
+                    getString(R.string.ubicacion29),
+                    getString(R.string.ubicacion30),
+                    getString(R.string.ubicacion32),
+                    getString(R.string.ubicacion33),
+                    getString(R.string.ubicacion34),
+                    getString(R.string.ubicacion35),
+                    getString(R.string.ubicacion36),
+                    getString(R.string.ubicacion38),
+                    getString(R.string.ubicacion40),
+                    getString(R.string.ubicacion41),
+                    getString(R.string.ubicacion42),
+                    getString(R.string.ubicacion43),
+                    getString(R.string.ubicacion44),
+                    getString(R.string.ubicacion3),
+                    getString(R.string.ubicacion5),
+                    getString(R.string.ubicacion6),
+                    getString(R.string.ubicacion8),
+                    getString(R.string.ubicacion9),
+                    getString(R.string.ubicacion10),
+                    getString(R.string.ubicacion12),
+                    getString(R.string.ubicacion15),
+                    getString(R.string.ubicacion16),
+                    getString(R.string.ubicacion18),
+                    getString(R.string.ubicacion20),
+                    getString(R.string.ubicacion23),
+                    getString(R.string.ubicacion24),
+                    getString(R.string.ubicacion26),
+                    getString(R.string.ubicacion37),
+                    getString(R.string.ubicacion39),
+                    getString(R.string.ubicacion45))
             }
 
             val randint = Random.nextInt(lista.size)
